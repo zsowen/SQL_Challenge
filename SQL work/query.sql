9028bdfc18933e898339a42eff74a53b7703e9c5
zsowen <zachary.owen.10@gmail.com>
2021-05-22 22:25:50 -0400

    complete query section

--- SQL work/query.sql
+++ SQL work/query.sql
@@ -1,1 +1,45 @@
-SELECT
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.sex, salaries.salary
+FROM employees
+INNER JOIN salaries ON employees.emp_no = salaries.emp_no;
+
+
+SELECT employees.first_name, employees.first_name, employees.hire_date
+FROM employees
+WHERE hire_date >= '1986-01-01' and hire_date <= '1986-12-31';
+
+
+SELECT dept_manager.dept_no, departments.dept_name, dept_manager.emp_no, employees.last_name, employees.first_name
+FROM dept_manager
+INNER JOIN departments ON dept_manager.dept_no = departments.dept_no
+INNER JOIN employees ON employees.emp_no = dept_manager.emp_no;
+
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no;
+
+
+SELECT employees.first_name, employees.last_name, employees.sex
+FROM employees
+WHERE first_name = 'Hercules' and last_name LIKE 'B%';
+
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales';
+
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM employees
+INNER JOIN dept_emp ON dept_emp.emp_no = employees.emp_no
+INNER JOIN departments ON departments.dept_no = dept_emp.dept_no
+WHERE departments.dept_name = 'Sales' OR departments.dept_name = 'Development';
+
+
+SELECT last_name, COUNT(last_name) AS "Name Count"
+FROM employees
+GROUP BY last_name
+Order BY "Name Count" DESC;
