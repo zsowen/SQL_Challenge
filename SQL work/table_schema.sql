0e9cd83983194614ef44a6819af64e883f32745c
zsowen <zachary.owen.10@gmail.com>
2021-05-22 21:19:12 -0400

    finish table schemata

--- SQL work/table_schema.sql
+++ SQL work/table_schema.sql
@@ -1,7 +1,9 @@
 DROP TABLE IF EXISTS departments;
 DROP TABLE IF EXISTS salaries;
 DROP TABLE IF EXISTS employees;
-
+DROP TABLE IF EXISTS dept_emp;
+DROP TABLE IF EXISTS dept_manager;
+DROP TABLE IF EXISTS titles;
 
 CREATE TABLE departments(
 	dept_no VARCHAR (20) NOT NULL,
@@ -23,8 +25,28 @@ CREATE TABLE employees(
 	last_name VARCHAR (30) NOT NULL,
 	sex VARCHAR (1) NOT NULL,
 	hire_date DATE NOT NULL,
-	PRIMARY KEY (emp_no)
-	CONSTRAINT fk_employee
-		FOREIGN KEY(emp_no)
-			REFERENCES salaries(emp_no)
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY(emp_no) REFERENCES salaries(emp_no)
+);
+
+CREATE TABLE dept_emp(
+	emp_no INT NOT NULL,
+	dept_no VARCHAR (30) NOT NULL,
+	PRIMARY KEY (emp_no),
+	FOREIGN KEY(emp_no) REFERENCES employees(emp_no),
+	FOREIGN KEY(dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager(
+	dept_no VARCHAR (30) NOT NULL,
+	emp_no INT NOT NULL,
+	PRIMARY KEY (dept_no, emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+);
+	
+CREATE TABLE titles(
+	title_id varchar(30) NOT NULL,
+	title varchar(30) NOT NULL,
+	PRIMARY KEY (title_id)
 );
